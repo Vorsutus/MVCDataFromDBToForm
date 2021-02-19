ce50253203e3815312b31d221e24a1e0727860b4
CMRoss <ChrisRoss@u.boisestate.edu>
2021-02-19 10:04:55 -0700

    Fixed Id null issue --Forgot to set Identity Specification to True so that it auto Increments

---
+++ MVCDemoDB/Publish Profiles/dbo.Employee.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Employee] (
+    [Id]           INT        NOT NULL IDENTITY,
+    [EmployeeId]   INT        NOT NULL,
+    [FirstName]    NCHAR (50) NOT NULL,
+    [LastName]     NCHAR (50) NOT NULL,
+    [EmailAddress] NCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
